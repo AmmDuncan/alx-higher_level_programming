9ac6ebf2756e9bd6f0341652e7ba8285e48668b4
ammduncan <ammielgyanyawson@gmail.com>
2022-08-23 13:20:20 +0000

    fix 1: change string conversion

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1:])
-last_digit = last_digit * -1 if (number < 0) else last_digit
-
+last_digit = number % 10
+last_digit = last_digit - 10 if (number < 0) else last_digit
 print(f"Last digit of {number} is {last_digit}", end=" ")
 
 if (last_digit > 5):
