c87afca31141ef8e1e7895e1e552e2e652224ad1
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-08-25 18:24:30 +0000

    fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 
+args = argv[1:]
+args_len = len(args)
 if __name__ == "__main__":
-    args = argv[1:]
-    args_len = len(args)
     print("{} arguments{}".format(args_len, ":" if args_len > 0 else '.'))
 
-    for index, arg in enumerate(args):
-        print("{}: {}".format(index + 1, arg))
+    for i in range(args_len):
+        print("{}: {}".format(i + 1, args[i]))
