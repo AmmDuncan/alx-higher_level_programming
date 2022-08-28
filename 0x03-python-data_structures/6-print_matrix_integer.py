34f195b862da681c39d642a9de939fef38f78a88
ammduncan <ammielgyanyawson@gmail.com>
2022-08-28 21:19:03 +0000

    fix 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
-    str = "{:d} {:d} {:d}"
+    str = " ".join(["{:d}"] * len(matrix[0]))
     for row in matrix:
         if len(row):
             print(str.format(*row))
