adef4cb14ed8d47f94fbfc9b5f86f779ccfa191e
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-08-31 20:59:15 +0000

    init 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    str_length = len(roman_string)
+    total = 0
+    for index, char in enumerate(roman_string):
+        is_end = index == str_length - 1
+        current_value = roman_values[char]
+        if is_end:
+            total += current_value
+        else:
+            next = roman_string[index + 1]
+            next_value = roman_values[next]
+            if next_value > current_value:
+                total -= current_value
+            else:
+                total += current_value
+            # print(f"{index}: {char}-> {current_value}")
+    return total
