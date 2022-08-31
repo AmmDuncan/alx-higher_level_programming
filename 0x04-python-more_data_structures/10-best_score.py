9acc3886286dd12063fdfacef777d9b721b8d7ee
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-08-31 19:59:40 +0000

    init 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not len(list(a_dictionary)):
+        return None
+    highest_key = list(a_dictionary)[0]
+    for key, value in a_dictionary.items():
+        if value > a_dictionary[highest_key]:
+            highest_key = key
+    return highest_key
