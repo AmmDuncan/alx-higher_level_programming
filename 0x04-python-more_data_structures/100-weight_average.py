4a21be02d9df06ae76f984b6e15aa51324150e00
ammduncan <ammielgyanyawson@gmail.com>
2022-08-31 21:13:32 +0000

    fix 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 def weight_average(my_list):
     if len(my_list) == 0 or my_list is None:
         return 0
-    
+
     total_weight = 0
     total_weighted_score = 0
-    
+
     for (score, weight) in my_list:
         total_weight += weight
         total_weighted_score += score * weight
