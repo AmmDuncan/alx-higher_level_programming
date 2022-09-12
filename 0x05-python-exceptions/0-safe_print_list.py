128bcba33ec71a609baf377b27e29a51c5b03afe
ammduncan <ammielgyanyawson@gmail.com>
2022-09-12 16:40:17 +0000

    fix 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while printed < x:
             print("{}".format(my_list[printed]), end="")
             printed += 1
+        print("\n", end="")
         return printed
     except:
         print("\n", end="")
