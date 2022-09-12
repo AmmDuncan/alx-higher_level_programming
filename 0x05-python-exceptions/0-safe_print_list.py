ad871b846a9684b33166ae386c8e92f2dd16d459
ammduncan <ammielgyanyawson@gmail.com>
2022-09-12 16:38:25 +0000

    init 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        while printed < x:
+            print("{}".format(my_list[printed]), end="")
+            printed += 1
+        return printed
+    except:
+        print("\n", end="")
+        return printed
