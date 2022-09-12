ba03de60c98d7dfd878cc962c0ce05fecb712806
ammduncan <ammielgyanyawson@gmail.com>
2022-09-12 16:59:19 +0000

    init 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = i = 0
+    while (printed < 5):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+            i += 1
+        except (ValueError, TypeError):
+            i += 1
+            continue
+    print("")
+    return printed
