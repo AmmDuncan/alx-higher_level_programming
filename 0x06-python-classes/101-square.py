aaa2378a6b395909da869f5ebe982a7bcc0d0e9c
ammduncan <ammielgyanyawson@gmail.com>
2022-09-13 22:02:51 +0000

    init 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -75,23 +75,7 @@ class Square:
 
     def __repr__(self):
         '''String representation of square'''
-        str_rep = ""
-        if self.size == 0:
-            str_rep += "\n"
-        else:
-            x, y = self.position
-            for _ in range(y):
-                str_rep += "\n"
-
-            for i in range(self.size):
-                for _ in range(x):
-                    str_rep += " "
-                for _ in range(self.size):
-                    str_rep += "#"
-                if i != self.size - 1:
-                    str_rep += "\n"
-
-        return str_rep
+        return f"Square({self.size}, {self.position})"
 
 
 def validate_size(size):
