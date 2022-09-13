7f00fe78f1fb07524e7a7b3ca812f3cbd2acf719
ammduncan <ammielgyanyawson@gmail.com>
2022-09-13 20:56:30 +0000

    fix 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+'''Define Square class with validation and area methods'''
+
+
+from wsgiref import validate
+
+
+class Square:
+    '''Square class'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''Initialize Square object'''
+        validate_size(size)
+        validate_position(position)
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        '''Get area of square'''
+        return self.__size**2
+
+    @property
+    def size(self):
+        '''Get size attr'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Set size attr'''
+        validate_size(value)
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        validate_position(value)
+        self.__position = value
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            x, y = self.position
+            for _ in range(y):
+                print()
+            for i in range(self.size):
+                for _ in range(x):
+                    print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print()
+
+
+def validate_size(size):
+    '''Validate size of square'''
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+
+def validate_position(position):
+    '''Validate position of square'''
+    if not isinstance(position, tuple):
+        raise_position_exception()
+    if len(position) != 2:
+        raise_position_exception()
+    x, y = position
+    if (x < 0 or y < 0):
+        raise_position_exception()
+
+
+def raise_position_exception():
+    '''Raise position exception'''
+    raise TypeError("position must be a tuple of 2 positive integers")
