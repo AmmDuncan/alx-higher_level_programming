e25c8531c0a916a96a48bbe4a85ab2db615aebe3
ammduncan <ammielgyanyawson@gmail.com>
2022-09-13 20:14:03 +0000

    init 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module to define a Square with size'''
+
+
+class Square:
+    '''Represents a Square Class'''
+
+    def __init__(self, size=0):
+        '''Initialize Square'''
+        self.__size = size
