e448919efba1af66f27cec9a757ed6725ea013ff
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-19 07:56:22 +0000

    init 0

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to try empty rectangle class"""
+
+Rectangle = __import__("0-rectangle").Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module containing rectangle class
+"""
+
+class Rectangle():
+    """Empty rectangle class"""
+    pass
