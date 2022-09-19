843d1bf7240943bcc60a009f33ae816167c06687
ammduncan <ammielgyanyawson@gmail.com>
2022-09-19 14:01:35 +0000

    fix 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,7 +81,7 @@ class Rectangle():
     @classmethod
     def square(cls, size=0):
         """Create square from Rectangle class"""
-        validate_length(size)
+        validate_length(size, "size")
         return cls(size, size)
 
 
