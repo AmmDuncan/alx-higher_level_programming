61b412ea5dfcc69e3ff90a42a63b1650e45eaa69
ammduncan <ammielgyanyawson@gmail.com>
2022-09-20 08:55:26 +0000

    exit status 1 on error

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -73,6 +73,7 @@ if __name__ == "__main__":
         N = int(sys.argv[1])
         if (N < 4):
             print("N must be at least 4")
+            sys.exit(1)
         board = generate_board(N)
         for i in range(N):
             solve_nqueen(N, i, passed, current, board)
@@ -82,3 +83,4 @@ if __name__ == "__main__":
                 print(res)
     except ValueError:
         print("N must be a number")
+        sys.exit(1)
