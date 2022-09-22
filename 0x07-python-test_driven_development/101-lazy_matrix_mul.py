f46af218dda4e618730277dd0292b9a7d56a296e
ammduncan <ammielgyanyawson@gmail.com>
2022-09-22 22:16:49 +0000

    change exception messages

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,7 +5,6 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    validate_args(m_a, m_b, "m_a", "m_b")
     mat_a = np.array(m_a)
     mat_b = np.array(m_b)
     return np.matmul(mat_a, mat_b)
