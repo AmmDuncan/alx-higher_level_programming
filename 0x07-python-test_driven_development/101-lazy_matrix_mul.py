1875866719c5cb45385ff4d638f737151257c273
ammduncan <ammielgyanyawson@gmail.com>
2022-09-22 22:17:52 +0000

    add docs

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """Multiply two matrices using numpy"""
     mat_a = np.array(m_a)
     mat_b = np.array(m_b)
     return np.matmul(mat_a, mat_b)
