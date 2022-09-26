d200d20ff199511ea57720be264e619c4a25340a
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-26 08:33:54 +0000

    init: inheritance 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""List all available attributes and methods
+    of an object"""
+
+def lookup(obj):
+    """Return list of available attrs and methods on obj"""
+    return dir(obj);
+
