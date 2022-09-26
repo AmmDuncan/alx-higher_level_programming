8f5d85cf32fb6ae17bd749ff7f722cecf01c3051
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-26 12:51:08 +0000

    fix: py-inheritance 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,8 +8,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiate Sqaure object"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
