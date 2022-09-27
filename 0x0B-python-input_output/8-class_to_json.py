1d5ae1cd4301cc5ef23a07b1f7417344045e8855
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-27 17:59:38 +0000

    fix: py-input/output 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Class to JSON"""
-import json
 
 
 def class_to_json(obj):
     """Convert class obj to json string"""
-    return json.dumps(obj.__dict__)
+    return str(obj.__dict__)
