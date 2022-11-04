6cc8331d306fbae8f7603c3efad16902f946b3dd
ammduncan <ammielgyanyawson@gmail.com>
2022-11-04 22:42:45 +0000

    fix 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Count shows by genre
-SELECT g.name as genre, COUNT(*) as number_of_shows
+SELECT g.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres g INNER JOIN tv_show_genres tvg
 ON g.id = tvg.genre_id
 GROUP BY g.name
