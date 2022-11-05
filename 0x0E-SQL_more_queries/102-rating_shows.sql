f887589e6d4efdf54d4be0551b5c765057ff77f5
ammduncan <ammielgyanyawson@gmail.com>
2022-11-05 14:51:49 +0000

    init 19

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,10 @@
+-- Shows with ratings
+SELECT 
+	title,
+	SUM(tsr.rate) as rating
+FROM
+	tv_shows t
+	INNER JOIN tv_show_ratings tsr
+	ON t.id = tsr.show_id
+GROUP BY t.title
+ORDER BY rating DESC;
