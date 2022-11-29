71c5a23a9564113d25bcaf3513d425bc1edd869d
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-11-29 23:20:23 +0000

    fix 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = process.argv;
-if (!args || args.length === 1) console.log('No argument');
-if (args && args.length === 2) console.log('Argument found');
-if (args && args.length > 2) console.log('Arguments found');
+if (!args || args.length <= 2) console.log('No argument');
+if (args && args.length === 3) console.log('Argument found');
+if (args && args.length > 3) console.log('Arguments found');
