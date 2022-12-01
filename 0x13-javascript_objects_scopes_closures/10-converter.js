c984a5237220b627bdb8f0fb649cd5c2a3272df1
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-01 09:28:38 +0000

    fix 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,9 +3,9 @@ exports.converter = function (base) {
   return (val) => {
     function getChar (v) {
       if (v < base) {
-        return '0123456789abcdef'[v % base];
+        return '0123456789abcdefghijklmnopqrst'[v % base];
       } else {
-        return getChar(Math.floor(val / base)) + '0123456789abcdef'[v % base];
+        return getChar(Math.floor(val / base)) + '0123456789abcdefghijklmnopqrst'[v % base];
       }
     }
     return getChar(val);
