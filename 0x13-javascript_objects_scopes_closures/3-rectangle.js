5f74682636dd51323e77933262bc559996e2bb5e
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-01 07:32:17 +0000

    int 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const rows = this.height;
+    const cols = this.width;
+
+    for (let i = 0; i < rows; i++) {
+      console.log(Array(cols).fill('X'));
+    }
+  }
+};
