b1db0803c31d0eacf10b001d914da3bb5a46ea0f
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-03 17:06:37 +0000

    fix 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,27 +2,29 @@
 '''
 Select all states from provided db 'states' table
 '''
-import MySQLdb
-import sys
 
-host = 'localhost'
-port = 3306
-user = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host=host,
-                     port=port,
-                     user=user,
-                     passwd=password,
-                     db=db_name)
-cur = db.cursor()
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-cur.execute('''
-            SELECT id, name
-            FROM states
-            ORDER BY id
-            ''')
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    db = MySQLdb.connect(host=host,
+                         port=port,
+                         user=user,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+
+    cur.execute('''
+                SELECT id, name
+                FROM states
+                ORDER BY id
+                ''')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
