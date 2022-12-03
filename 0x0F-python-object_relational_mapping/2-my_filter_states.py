0f3bcffd2a796e753c3b457801febe44e1290766
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-03 17:44:00 +0000

    init 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    select all states in the states table where
+    name matches provide argument
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=password, db=db_name)
+    cur = db.cursor()
+
+    cur.execute('''
+                SELECT id, name
+                FROM states
+                WHERE name = %s
+                ORDER BY id;
+                ''', (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
