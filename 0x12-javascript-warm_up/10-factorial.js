5b9822f0f4f61f8620738addd3a491e995b232da
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-30 11:56:13 +0000

    fix 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,9 +7,9 @@ const n = Number(process.argv[2]);
 
 function factorial (n) {
   if ([0, 1, NaN].includes(n)) {
-    return n;
+    return 1;
   }
-  return factorial(n - 1) + factorial(n - 2);
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(n));
