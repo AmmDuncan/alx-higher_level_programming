a8d0d93a13ac395dab726679673be393faaac053
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-30 12:32:09 +0000

    init 103

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs/promises');
+
+const source1 = process.argv[2];
+const source2 = process.argv[3];
+const destination = process.argv[4];
+
+async function main() {
+  const content1 = await fs.readFile(source1, {flag: 'r'});
+  const content2 = await fs.readFile(source2, {flag: 'r'});
+  const resFile = await fs.open(destination, 'w+')
+  await resFile.write(content1.toString() + content2.toString());
+}
+
+main();
