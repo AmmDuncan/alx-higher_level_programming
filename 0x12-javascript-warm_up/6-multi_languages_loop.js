203dc811ea8dc271f2d893105e8dff2922483468
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-30 10:29:52 +0000

    init 6 multi languages loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * loop through array and print members
+ */
+
+const sentences = [
+  'C is fun',
+  'Python is cool',
+  'Javascript is amazing'
+];
+
+for (let i = 0; i < sentences.length; i++) {
+  console.log(sentences[i]);
+}
